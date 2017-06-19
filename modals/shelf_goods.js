d3e38204a4eb113d56676ef4b29d94d347be22be
ZhangRZ <occultskyrong@163.com>
2017-06-19 19:43:36 +0800

    update shelf_goods modal

--- modals/shelf_goods.js
+++ modals/shelf_goods.js
@@ -5,3 +5,35 @@
  */
 
 "use strict";
+
+class Shelf_Goods {
+    constructor(good_id) {
+        this.good_id = parseInt(good_id);
+    }
+
+    // 写:商品名称
+    setTitle(title) {
+        this.title = title;
+        this.title_ik_smart = title;
+        this.title_ik_max = title;
+        this.title_pinyin_only = title;
+        this.title_pinyin_full = title;
+    }
+
+    // 写:更新时间
+    setUpdateTS(ts) {
+        this.update_timestamp = parseInt(ts);
+    }
+
+    // 写:创建时间
+    setCreateTS(ts) {
+        this.create_timestamp = parseInt(ts);
+    }
+
+    // 写:时间标记
+    setUpdatedAt() {
+        this.update_at = new Date();
+    }
+}
+
+module.exports = Shelf_Goods;

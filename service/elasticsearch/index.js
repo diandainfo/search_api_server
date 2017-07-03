50ec933078919b9ee9c4d293494594af35709e78
zrz <occultskyrong@163.com>
2017-07-03 17:16:22 +0800

    update elasticsearch service

--- service/elasticsearch/index.js
+++ service/elasticsearch/index.js
@@ -6,15 +6,42 @@
 
 "use strict";
 
-const esUtils = require('../../utils/elasticsearch');
+const client = require('../../utils/elasticSearch').client.dn
+    , config = require('../../config')
+    , methodUtils = require('../../utils/method')
+    , sLog = GLO.logger('es-start');
 
 const _ = {
-    // 检查节点
-    check: esUtils.check
+    ping: ()=>
+        new Promise((resolve, reject)=>
+            client
+                .ping({
+                    requestTimeout: 10000 // 延迟相应时间为10s
+                })
+                .then(()=>resolve())
+                .catch((error)=> {
+                    sLog.error(error);
+                    reject('连接ES节点失败');
+                })
+        )
+
+    // 获取本机IP，确认是否需要检查节点
+    , needCheck: ()=> config.es.check && config.es.check_ip === methodUtils.getLocalIp()
+    , check: ()=>
+        new Promise((resolve, reject)=> {
+            _.ping()                                        // 连接es节点
+                .then(()=> {
+                    GLO.log('启动检查 - ElasticSearch 节点连接成功', 'start');
+                    // console.info(_.needCheck());
+                    resolve();
+                })
+                .catch(reject);
+        })
 
     // TODO 定时任务
     , schedule: require('./schedule')
 
+    // TODO 创建索引
 
     // 启动任务
     , run: ()=>
